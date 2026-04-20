99e35be95bea9efc30eccb607b4aef4ef75b53fc
Nicolas Biercher <biercher.nicolas@outlook.de>
2026-04-20 08:28:51 +0200

    latest

--- public/teamchef_anmelden.php
+++ public/teamchef_anmelden.php
@@ -1,3 +1,4 @@
+<!-- Nicolas Biercher Beginn -->
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -5,28 +6,64 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Teamchef anmelden</title>
 </head>
+
 <body>
+    <h1>Teamchef anmelden</h1>
+    <form action="" method="POST">
+        <fieldset>
+            <legend>Login-Daten unten eintragen</legend>
+            <p>
+                <label for="teamchef_loginname">Loginname</label><br>
+                <input id="teamchef_loginname" name="teamchef_loginname">
+            </p>
+            <p>
+                <label for="teamchef_kennwort">Kennwort</label><br>
+                <input id="teamchef_kennwort" name="teamchef_kennwort" type="password">
+            </p>
 
-<h1>Teamchef anmelden</h1>
- <form>
-
- <fieldset>
-        <legend>Login-Daten unten eintragen</legend>
-         <form>
-        <p>
-            <label for="teamchef_loginname">Loginname</label><br>
-            <input id="teamchef_loginname" name="teamchef_loginname">
-        </p>
-        <p>
-            <label for="teamchef_kennwort">Kennwort</label><br>
-            <input id="teamchef_kennwort" name="teamchef_kennwort" type="password">
-        </p>
-
-        <p><input type="submit" value="Speichern"></p>
-        <p><a href="index.php">Zurück zur Startseite</a></p>
+            <p><input type="submit" value="Speichern"></p>
+            <p><a href="index.php">Zurück zur Startseite</a></p>
+        </fieldset>
     </form>
 
- </fieldset>
+    <?php
+
+    include_once 'dbh.php';
+
+    class Teamchef extends Dbh
+    {
+        public function teamchefAnmelden($teamchef_loginname, $teamchef_kennwort)
+        {
+            $sql = "SELECT * FROM Teamchef WHERE TeamchefLoginName = ?";
+            $stmt = $this->connect()->prepare($sql);
+            $stmt->execute([$teamchef_loginname]);
+            $result = $stmt->fetch();
+
+            if (!$result) {
+                echo "Bitte registriere dich zuerst!";
+                return;
+            }
+
+            if ($result['Kennwort'] === $teamchef_kennwort) {
+                echo "Willkommen, " . $result['TeamchefLoginName'] . "!";
+            } else {
+                echo "Falsches Kennwort!";
+            }
+        }
+    }
+
+    if (isset($_POST['login'])) {
+        $teamchef_loginname = $_POST['teamchef_loginname'];
+        $teamchef_kennwort = $_POST['teamchef_kennwort'];
+
+        $teamchef_einloggen = new teamchef();
+        $teamchef_einloggen->teamchefAnmelden($teamchef_loginname, $teamchef_kennwort);
+    }
+    ?>
+
 
 </body>
+
 </html>
+
+<!-- Nicolas Biercher Ende -->
