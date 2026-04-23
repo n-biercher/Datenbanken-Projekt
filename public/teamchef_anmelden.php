3502fa183b0891b3011279947906d8e0101ad1ba
Nicolas Biercher <biercher.nicolas@outlook.de>
2026-04-23 14:19:59 +0200

    latest

--- public/teamchef_anmelden.php
+++ public/teamchef_anmelden.php
@@ -64,7 +64,7 @@ class TeamchefLogin extends Dbh
             $_SESSION['vorname'] = $user['Vorname'];
             $_SESSION['nachname'] = $user['Nachname'];
 
-            header("Location: teamchef_startseite.php"); //habe ich umgeleitet, für die Fahreranmeldung 
+            header("Location: index.php"); //habe ich umgeleitet, für die Fahreranmeldung 
             exit();
 
         } else {
