89a16a1195dfddfa0b0779c52b8aa17c487086e3
Nicolas Biercher <biercher.nicolas@outlook.de>
2026-04-23 12:11:23 +0200

    latest

--- public/team_verwalten.php
+++ public/team_verwalten.php
@@ -1,4 +1,3 @@
-<!-- Nicolas Biercher Beginn -->
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -27,184 +26,18 @@ class TeamVerwaltung extends Dbh
         $db_verbindung = $this->connect();
 
         $query = $db_verbindung->prepare("
-            SELECT
-                `Mitarbeiter-ID` AS mitarbeiter_id,
-                Vorname AS vorname,
-                Nachname AS nachname,
-                `Straße` AS strasse,
-                Hausnummer AS hausnummer,
-                Telefonnummer AS telefonnummer,
-                PLZ AS plz,
-                Ort AS ort
+            SELECT *
             FROM Fahrer
             WHERE Teamname = ?
-            ORDER BY `Mitarbeiter-ID` ASC
         ");
         $query->execute([$teamname]);
 
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
-
-    public function holeFahrer($mitarbeiter_id, $teamname)
-    {
-        $db_verbindung = $this->connect();
-
-        $query = $db_verbindung->prepare("
-            SELECT
-                `Mitarbeiter-ID` AS mitarbeiter_id,
-                Vorname AS vorname,
-                Nachname AS nachname,
-                `Straße` AS strasse,
-                Hausnummer AS hausnummer,
-                Telefonnummer AS telefonnummer,
-                PLZ AS plz,
-                Ort AS ort
-            FROM Fahrer
-            WHERE `Mitarbeiter-ID` = ? AND Teamname = ?
-        ");
-        $query->execute([$mitarbeiter_id, $teamname]);
-
-        return $query->fetch(PDO::FETCH_ASSOC);
-    }
-
-    private function sindFahrerdatenGueltig($mitarbeiter_id, $vorname, $nachname, $strasse, $hausnummer, $telefonnummer, $plz, $ort)
-    {
-        if ($mitarbeiter_id === '' || !ctype_digit($mitarbeiter_id)) {
-            return "Die Mitarbeiter-ID muss eine ganze Zahl sein";
-        }
-
-        if (trim($vorname) === '') {
-            return "Vorname darf nicht leer sein";
-        }
-
-        if (trim($nachname) === '') {
-            return "Nachname darf nicht leer sein";
-        }
-
-        if (trim($strasse) === '') {
-            return "Straße darf nicht leer sein";
-        }
-
-        if (trim($hausnummer) === '') {
-            return "Hausnummer darf nicht leer sein";
-        }
-
-        if (trim($telefonnummer) === '') {
-            return "Telefonnummer darf nicht leer sein";
-        }
-
-        if (!preg_match('/^[0-9]{5}$/', $plz)) {
-            return "Die PLZ muss aus genau 5 Ziffern bestehen";
-        }
-
-        if (trim($ort) === '') {
-            return "Ort darf nicht leer sein";
-        }
-
-        return true;
-    }
-
-    public function neuerFahrer($mitarbeiter_id, $vorname, $nachname, $strasse, $hausnummer, $telefonnummer, $plz, $ort, $teamname)
-    {
-        $pruefung = $this->sindFahrerdatenGueltig($mitarbeiter_id, $vorname, $nachname, $strasse, $hausnummer, $telefonnummer, $plz, $ort);
-        if ($pruefung !== true) {
-            return $pruefung;
-        }
-
-        $db_verbindung = $this->connect();
-
-        $vorhanden_query = $db_verbindung->prepare("
-            SELECT 1
-            FROM Fahrer
-            WHERE `Mitarbeiter-ID` = ? AND Teamname = ?
-        ");
-        $vorhanden_query->execute([$mitarbeiter_id, $teamname]);
-
-        if ($vorhanden_query->fetch()) {
-            return "Diese Mitarbeiter-ID existiert in deinem Team bereits";
-        }
-
-        $insert_query = $db_verbindung->prepare("
-            INSERT INTO Fahrer
-            (`Mitarbeiter-ID`, Vorname, Nachname, `Straße`, Hausnummer, Telefonnummer, PLZ, Ort, Teamname)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ");
-        $insert_query->execute([
-            $mitarbeiter_id,
-            $vorname,
-            $nachname,
-            $strasse,
-            $hausnummer,
-            $telefonnummer,
-            $plz,
-            $ort,
-            $teamname
-        ]);
-
-        return "Fahrer wurde erfolgreich angelegt";
-    }
-
-    public function fahrerAendern($mitarbeiter_id, $vorname, $nachname, $strasse, $hausnummer, $telefonnummer, $plz, $ort, $teamname)
-    {
-        $pruefung = $this->sindFahrerdatenGueltig($mitarbeiter_id, $vorname, $nachname, $strasse, $hausnummer, $telefonnummer, $plz, $ort);
-        if ($pruefung !== true) {
-            return $pruefung;
-        }
-
-        $db_verbindung = $this->connect();
-
-        $update_query = $db_verbindung->prepare("
-            UPDATE Fahrer
-            SET
-                Vorname = ?,
-                Nachname = ?,
-                `Straße` = ?,
-                Hausnummer = ?,
-                Telefonnummer = ?,
-                PLZ = ?,
-                Ort = ?
-            WHERE `Mitarbeiter-ID` = ? AND Teamname = ?
-        ");
-        $update_query->execute([
-            $vorname,
-            $nachname,
-            $strasse,
-            $hausnummer,
-            $telefonnummer,
-            $plz,
-            $ort,
-            $mitarbeiter_id,
-            $teamname
-        ]);
-
-        return "Fahrerdaten wurden erfolgreich geändert";
-    }
-
-    public function fahrerLoeschen($mitarbeiter_id, $teamname)
-    {
-        $db_verbindung = $this->connect();
-
-        try {
-            $delete_query = $db_verbindung->prepare("
-                DELETE FROM Fahrer
-                WHERE `Mitarbeiter-ID` = ? AND Teamname = ?
-            ");
-            $delete_query->execute([$mitarbeiter_id, $teamname]);
-
-            if ($delete_query->rowCount() === 0) {
-                return "Fahrer konnte nicht gelöscht werden";
-            }
-
-            return "Fahrer wurde erfolgreich gelöscht";
-        } catch (PDOException $e) {
-            return "Fahrer konnte nicht gelöscht werden, da noch abhängige Trainings- oder Renndaten vorhanden sind";
-        }
-    }
 }
 
 if (!isset($_SESSION['teamchef_loginname'])) {
-    echo "<p>Bitte zuerst als Teamchef anmelden.</p>";
-    echo '<p><a href="teamchef_anmelden.php">Zum Login</a></p>';
+    echo "Bitte zuerst als Teamchef anmelden.";
     exit;
 }
 
@@ -214,99 +47,19 @@ $verwaltung = new TeamVerwaltung();
 $team = $verwaltung->holeTeamnameVomTeamchef($teamchef_loginname);
 
 if (!$team) {
-    echo "<p>Es wurde kein Team zu diesem Teamchef gefunden.</p>";
-    echo '<p><a href="index.php">Zurück</a></p>';
+    echo "Kein Team gefunden.";
     exit;
 }
 
 $teamname = $team['Teamname'];
-$meldung = "";
-$bearbeitungsmodus = false;
-
-$formular_daten = [
-    'mitarbeiter_id' => '',
-    'vorname' => '',
-    'nachname' => '',
-    'strasse' => '',
-    'hausnummer' => '',
-    'telefonnummer' => '',
-    'plz' => '',
-    'ort' => ''
-];
-
-if (isset($_POST['speichern'])) {
-    $mitarbeiter_id = trim($_POST['mitarbeiter_id']);
-    $vorname = trim($_POST['vorname']);
-    $nachname = trim($_POST['nachname']);
-    $strasse = trim($_POST['strasse']);
-    $hausnummer = trim($_POST['hausnummer']);
-    $telefonnummer = trim($_POST['telefonnummer']);
-    $plz = trim($_POST['plz']);
-    $ort = trim($_POST['ort']);
-    $modus = $_POST['modus'];
-
-    if ($modus === 'bearbeiten') {
-        $meldung = $verwaltung->fahrerAendern(
-            $mitarbeiter_id,
-            $vorname,
-            $nachname,
-            $strasse,
-            $hausnummer,
-            $telefonnummer,
-            $plz,
-            $ort,
-            $teamname
-        );
-        $bearbeitungsmodus = true;
-    } else {
-        $meldung = $verwaltung->neuerFahrer(
-            $mitarbeiter_id,
-            $vorname,
-            $nachname,
-            $strasse,
-            $hausnummer,
-            $telefonnummer,
-            $plz,
-            $ort,
-            $teamname
-        );
-    }
-
-    if ($meldung !== "Fahrer wurde erfolgreich angelegt" && $meldung !== "Fahrerdaten wurden erfolgreich geändert") {
-        $formular_daten['mitarbeiter_id'] = $mitarbeiter_id;
-        $formular_daten['vorname'] = $vorname;
-        $formular_daten['nachname'] = $nachname;
-        $formular_daten['strasse'] = $strasse;
-        $formular_daten['hausnummer'] = $hausnummer;
-        $formular_daten['telefonnummer'] = $telefonnummer;
-        $formular_daten['plz'] = $plz;
-        $formular_daten['ort'] = $ort;
-    }
-}
-
-if (isset($_POST['loeschen'])) {
-    $mitarbeiter_id = $_POST['mitarbeiter_id'];
-    $meldung = $verwaltung->fahrerLoeschen($mitarbeiter_id, $teamname);
-}
-
-if (isset($_GET['bearbeiten'])) {
-    $fahrer = $verwaltung->holeFahrer($_GET['bearbeiten'], $teamname);
-
-    if ($fahrer) {
-        $bearbeitungsmodus = true;
-        $formular_daten['mitarbeiter_id'] = $fahrer['mitarbeiter_id'];
-        $formular_daten['vorname'] = $fahrer['vorname'];
-        $formular_daten['nachname'] = $fahrer['nachname'];
-        $formular_daten['strasse'] = $fahrer['strasse'];
-        $formular_daten['hausnummer'] = $fahrer['hausnummer'];
-        $formular_daten['telefonnummer'] = $fahrer['telefonnummer'];
-        $formular_daten['plz'] = $fahrer['plz'];
-        $formular_daten['ort'] = $fahrer['ort'];
-    }
-}
-
 $alle_fahrer = $verwaltung->holeAlleFahrer($teamname);
+
+function wert($array, $key)
+{
+    return isset($array[$key]) ? htmlspecialchars($array[$key]) : '';
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
@@ -321,75 +74,7 @@ $alle_fahrer = $verwaltung->holeAlleFahrer($teamname);
 <p>Angemeldet als Teamchef: <?php echo htmlspecialchars($teamchef_loginname); ?></p>
 <p>Team: <?php echo htmlspecialchars($teamname); ?></p>
 
-<?php if ($meldung !== ""): ?>
-    <p><?php echo htmlspecialchars($meldung); ?></p>
-<?php endif; ?>
-
-<form action="" method="POST">
-    <fieldset>
-        <legend><?php echo $bearbeitungsmodus ? "Fahrer bearbeiten" : "Neuen Fahrer anlegen"; ?></legend>
-
-        <input type="hidden" name="modus" value="<?php echo $bearbeitungsmodus ? 'bearbeiten' : 'neu'; ?>">
-
-        <p>
-            <label>Mitarbeiter-ID</label><br>
-            <input
-                name="mitarbeiter_id"
-                value="<?php echo htmlspecialchars($formular_daten['mitarbeiter_id']); ?>"
-                <?php echo $bearbeitungsmodus ? 'readonly' : 'required'; ?>
-            >
-        </p>
-
-        <p>
-            <label>Vorname</label><br>
-            <input name="vorname" value="<?php echo htmlspecialchars($formular_daten['vorname']); ?>" required>
-        </p>
-
-        <p>
-            <label>Nachname</label><br>
-            <input name="nachname" value="<?php echo htmlspecialchars($formular_daten['nachname']); ?>" required>
-        </p>
-
-        <p>
-            <label>Straße</label><br>
-            <input name="strasse" value="<?php echo htmlspecialchars($formular_daten['strasse']); ?>" required>
-        </p>
-
-        <p>
-            <label>Hausnummer</label><br>
-            <input name="hausnummer" value="<?php echo htmlspecialchars($formular_daten['hausnummer']); ?>" required>
-        </p>
-
-        <p>
-            <label>Telefonnummer</label><br>
-            <input name="telefonnummer" value="<?php echo htmlspecialchars($formular_daten['telefonnummer']); ?>" required>
-        </p>
-
-        <p>
-            <label>PLZ</label><br>
-            <input name="plz" maxlength="5" value="<?php echo htmlspecialchars($formular_daten['plz']); ?>" required>
-        </p>
-
-        <p>
-            <label>Ort</label><br>
-            <input name="ort" value="<?php echo htmlspecialchars($formular_daten['ort']); ?>" required>
-        </p>
-
-        <p>
-            <input type="submit" name="speichern" value="Speichern">
-        </p>
-
-        <p>
-            <a href="team_verwalten.php">Neuen Fahrer erfassen</a>
-        </p>
-
-        <p>
-            <a href="index.php">Zurück</a>
-        </p>
-    </fieldset>
-</form>
-
-<h2>Alle Fahrer im Team</h2>
+<h2>Alle Fahrer</h2>
 
 <table border="1" cellpadding="6" cellspacing="0">
     <tr>
@@ -401,37 +86,31 @@ $alle_fahrer = $verwaltung->holeAlleFahrer($teamname);
         <th>Telefonnummer</th>
         <th>PLZ</th>
         <th>Ort</th>
-        <th>Aktion</th>
+        <th>Teamname</th>
     </tr>
 
     <?php if (count($alle_fahrer) > 0): ?>
         <?php foreach ($alle_fahrer as $fahrer): ?>
             <tr>
-                <td><?php echo htmlspecialchars($fahrer['mitarbeiter_id']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['vorname']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['nachname']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['strasse']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['hausnummer']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['telefonnummer']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['plz']); ?></td>
-                <td><?php echo htmlspecialchars($fahrer['ort']); ?></td>
-                <td>
-                    <a href="team_verwalten.php?bearbeiten=<?php echo urlencode($fahrer['mitarbeiter_id']); ?>">Bearbeiten</a>
-
-                    <form action="" method="POST" style="display:inline;">
-                        <input type="hidden" name="mitarbeiter_id" value="<?php echo htmlspecialchars($fahrer['mitarbeiter_id']); ?>">
-                        <input type="submit" name="loeschen" value="Löschen" onclick="return confirm('Soll dieser Fahrer wirklich gelöscht werden?');">
-                    </form>
-                </td>
+                <td><?php echo wert($fahrer, 'Mitarbeiter-ID'); ?></td>
+                <td><?php echo wert($fahrer, 'Vorname'); ?></td>
+                <td><?php echo wert($fahrer, 'Nachname'); ?></td>
+                <td><?php echo wert($fahrer, 'Straße'); ?></td>
+                <td><?php echo wert($fahrer, 'Hausnummer'); ?></td>
+                <td><?php echo wert($fahrer, 'Telefonnummer'); ?></td>
+                <td><?php echo wert($fahrer, 'PLZ'); ?></td>
+                <td><?php echo wert($fahrer, 'Ort'); ?></td>
+                <td><?php echo wert($fahrer, 'Teamname'); ?></td>
             </tr>
         <?php endforeach; ?>
     <?php else: ?>
         <tr>
-            <td colspan="9">Noch keine Fahrer im Team vorhanden</td>
+            <td colspan="9">Keine Fahrer gefunden</td>
         </tr>
     <?php endif; ?>
 </table>
 
+<p><a href="index.php">Zurück</a></p>
+
 </body>
 </html>
-<!-- Nicolas Biercher Ende -->
