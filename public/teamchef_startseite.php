7fa1d80ab0e001b313403dcc5bee9cc525c27e20
lena2701 <lenastrohmenger@gmx.de>
2026-04-25 15:35:33 +0200

    Refactor teamchef start page for improved readability and maintainability

--- public/teamchef_startseite.php
+++ public/teamchef_startseite.php
@@ -19,10 +19,66 @@ class Rennen extends Dbh
         $stmt = $this->connect()->query($sql);
         return $stmt->fetchAll();
     }
+
+    public function alleFahrerHolen($teamchef_loginname)
+    {
+        $sql = "SELECT Fahrer.`Mitarbeiter-ID`, Fahrer.Vorname, Fahrer.Nachname
+        FROM Fahrer
+        JOIN Team ON Fahrer.Teamname = Team.Teamname
+        WHERE Team.TeamchefLoginName = ?";
+
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$teamchef_loginname]);
+        return $stmt->fetchAll();
+    }
+
+    public function fahrerAnmelden($mitarbeiter_id, $renn_id)
+    {
+        $sql_team = "SELECT Teamname FROM Fahrer WHERE `Mitarbeiter-ID` = ?";
+        $stmt_team = $this->connect()->prepare($sql_team);
+        $stmt_team->execute([$mitarbeiter_id]);
+        $fahrer = $stmt_team->fetch();
+
+        $sql_insert = "INSERT INTO Teilnahme (MitarbeiterId, Teamname, RennId)
+                   VALUES (?, ?, ?)";
+        $stmt_insert = $this->connect()->prepare($sql_insert);
+        $stmt_insert->execute([$mitarbeiter_id, $fahrer['Teamname'], $renn_id]);
+    }
 }
+
 
 $rennen_objekt = new Rennen();
 $alle_rennen = $rennen_objekt->alleRennenHolen();
+$alle_fahrer = $rennen_objekt->alleFahrerHolen($_SESSION['teamchef_loginname']);
+
+$renn_id = "";
+$anzahl_fahrer = 0;
+$formular_anzeigen = false;
+$fehlermeldung = "";
+
+if (isset($_POST['rennen_auswaehlen'])) {
+    if (isset($_POST['rennid']) && isset($_POST['anzahl_fahrer'])) {
+        $renn_id = $_POST['rennid'];
+        $anzahl_fahrer = (int) $_POST['anzahl_fahrer'];
+        $formular_anzeigen = true;
+    } else {
+        $fehlermeldung = "Bitte wähle ein Rennen aus und gib die Anzahl der Fahrer an.";
+    }
+}
+
+if (isset($_POST['fahrer_anmelden'])) {
+
+    $renn_id = $_POST['renn_id'];
+
+    for ($i = 1; $i <= $_POST['anzahl_fahrer']; $i++) {
+
+        $mitarbeiter_id = $_POST['fahrer_' . $i];
+
+        $rennen_objekt->fahrerAnmelden($mitarbeiter_id, $renn_id);
+
+    }
+
+}
 
 ?>
 <!DOCTYPE html>
@@ -41,6 +97,8 @@ $alle_rennen = $rennen_objekt->alleRennenHolen();
     <p>Du bist jetzt eingeloggt.</p>
 
     <p><a href="logout.php">Logout</a></p>
+    <p><a href="team_verwalten.php">Team verwalten</a></p> <!-- Nicolas Biercher -->
+
     <h2>Fahrer zu einem Rennen anmelden</h2>
 
     <form action="" method="POST">
@@ -64,13 +122,61 @@ $alle_rennen = $rennen_objekt->alleRennenHolen();
                 echo '<p>Es sind aktuell keine Rennen vorhanden.</p>';
             }
             ?>
+            <p>
+                <label for="anzahl_fahrer">Anzahl Fahrer anmelden:</label><br>
+                <input id="anzahl_fahrer" name="anzahl_fahrer" type="number" min="1" max="10" required value="<?php echo $anzahl_fahrer; ?>">
+            </p>
 
-            <p><input type="submit" name="rennen_auswaehlen" value="Rennen auswählen"></p>
+            <p><input type="submit" name="rennen_auswaehlen" value="Rennen auswählen" required></p>
+
+            <?php
+            if (!empty($fehlermeldung)) {
+                echo '<p style="color: red;">' . $fehlermeldung . '</p>';
+            }
+            ?>
+
+            <?php
+            if ($formular_anzeigen) { ?>
+                <h3>Fahrer für Rennen <?php echo $renn_id; ?> eingeben</h3>
+
+                <input type="hidden" name="renn_id" value="<?php echo $renn_id; ?>">
+                <input type="hidden" name="anzahl_fahrer" value="<?php echo $anzahl_fahrer; ?>">
+
+                <table border="1" cellpadding="8">
+                    <tr>
+                        <th>Nr.</th>
+                        <th>Fahrername</th>
+                    </tr>
+
+                    <?php
+                    for ($i = 1; $i <= $anzahl_fahrer; $i++) {
+                        echo '<tr>';
+                        echo '<td>' . $i . '</td>';
+
+                        echo '<td>';
+                        echo '<select name="fahrer_' . $i . '" required>';
+                        echo '<option value="">Bitte wählen</option>';
+
+                        foreach ($alle_fahrer as $fahrer) {
+                            echo '<option value="' . $fahrer['Mitarbeiter-ID'] . '">';
+                            echo $fahrer['Vorname'] . ' ' . $fahrer['Nachname'];
+                            echo '</option>';
+                        }
+
+                        echo '</select>';
+                        echo '</td>';
+                        echo '</tr>';
+                    }
+                    ?>
+                </table>
+
+                <p><input type="submit" name="fahrer_anmelden" value="Fahrer anmelden"></p>
+            <?php } ?>
+
+
         </fieldset>
     </form>
 </body>
-
-
 
 </html>
 
